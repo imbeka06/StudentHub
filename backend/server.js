916f6d1d90d88243b8e26eeb222b253a4d84979f
brianngumbau <brianngumbau9075@gmail.com>
2025-07-22 11:50:30 +0300

    Initial backend setup: Express, PostgreSQL, Firebase Admin, .env config

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const { Pool } = require('pg');
+const admin = require('firebase-admin');
+const fs = require('fs');
+const path = require('path');
+
+//loading .env
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+//PostgreSQL config
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+app.set('db', pool)
+
+//Firebase Admin Initialization
+const serviveAccount = require('./firebase-config.json');
+admin.initializeApp({
+    credential: admin.credential.cert(serviveAccount),
+});
+
+//Routes
+app.get('/ping', (req, res) => {
+    res.json({message: 'Backend is alive'});
+});
+
+// start server
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
